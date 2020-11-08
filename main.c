19198d47a4f7d579a95b0d7b2c0e8ca87912fb9d
dvirbz <58605853+dvirbz@users.noreply.github.com>
2020-11-08 11:12:47 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		printf("Mismatcing number of arguments: %d", argc);
+		return -1;
+	}
+	FILE* forest_string;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
